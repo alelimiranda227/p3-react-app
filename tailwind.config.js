5f816cb0109cc51bc3e6556378b6b11b32619feb
alelimiranda <alelimiranda227@gmail.com>
2024-08-18 14:53:17 +0800

    Fix background image path and update Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       backgroundImage: {
-        'lcct-pic': "url('./src/assets/images/LCCT_Building_1.png')"
-      }
+        'lcct-pic': "url('/assets/images/LCCT_Building_1.png')",
+      },
     },
     
   },
